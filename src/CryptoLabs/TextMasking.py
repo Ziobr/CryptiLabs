9a8a41f16443ee72cb19d2176bbcbb0cb7a9e167
Ziobr <equus.quagga@gmail.com>
2015-04-03 00:20:07 +0400

    Firs prototype

---
+++ src/CryptoLabs/TextMasking.py
@@ -0,0 +1,25 @@
+﻿# coding=utf-8
+'''
+Created on 02 апр. 2015 г.
+
+@author: Ziobr
+'''
+import codecs
+
+def maskText(text='', symbolForReplace='', symbolToShow=''):
+    intab = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    outtab = ''
+    for s in intab:
+        if s == symbolForReplace:
+            outtab = outtab + symbolToShow
+        else:
+            outtab = outtab + '*'
+    translationTab = str.maketrans(intab, outtab)
+    return text.translate(translationTab)
+
+if __name__ == '__main__':
+    f = codecs.open('..\..\Шифр.txt', 'r', 'utf-8')
+    text = f.read()
+    print(text)
+    print(maskText(text, 'и', 'я'))
+    f.close()
